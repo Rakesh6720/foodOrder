2bfb2a21d45b3f2be5ebdedf892f0e4f88852756
Rakesh Baruah <rbaruah@gmail.com>
2021-12-22 09:45:30 -0600

    initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import HeaderCartButton from "./HeaderCartButton";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img
+          src="https://images.unsplash.com/photo-1471193945509-9ad0617afabf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+          alt="food"
+        />
+      </div>
+    </>
+  );
+};
+
+export default Header;
